8cc3f550581668ba05f82e43ece4e1ddef32951f
Alexandra Barina <alexandra.barina@softvision.ro>
2019-07-22 13:47:51 +0300

    edited JS file

--- JS/scripts.js
+++ JS/scripts.js
@@ -9,25 +9,27 @@
 
 /***************  MAIN FUNCTION  ****************/
 (function() {
+
     const firstName = document.getElementById('firstName');
     const lastName = document.getElementById('lastName');
     const email = document.getElementById('email');
     const checkoutBtn = document.getElementById('checkout');
-​
-    // firstName
+    // firstName 
+    
     firstName.addEventListener('change', function(event){
+        console.log("dfgdfgdfg");
         var value = document.getElementById("firstName").value;
         if (value == "") {
             document.getElementById("firstName").setAttribute("style", "border: 1px solid red;");
         } else {
             document.getElementById("firstName").setAttribute("style", "border: 1px solid #777777;");
         }
-​
+        
         // if the 3 fields aren't empty enable checkout button
         let fname = checkName(firstName.value);
         let lname = checkName(lastName.value);
         let mail = validateEmail(email.value);
-​
+        
         if(fname && lname && mail === true){
             checkoutBtn.disabled = false;
             document.getElementById('custom-error').style.display = "none";
@@ -35,7 +37,7 @@
             document.getElementById('custom-error').style.display = "block";
         }
     });
-​
+    
     // lastName
     lastName.addEventListener('change', function(event){
         var value = document.getElementById("lastName").value;
@@ -44,12 +46,12 @@
         } else {
             document.getElementById("lastName").setAttribute("style", "border: 1px solid #777777;");
         }
-​
+        
         // if the 3 fields aren't empty enable checkout button
         let fname = checkName(firstName.value);
         let lname = checkName(lastName.value);
         let mail = validateEmail(email.value);
-​
+        
         if(fname && lname && mail === true){
             checkoutBtn.disabled = false;
             document.getElementById('custom-error').style.display = "none";
@@ -57,7 +59,7 @@
             document.getElementById('custom-error').style.display = "block";
         }
     });
-​
+    
     // email 
     email.addEventListener('change', function(event){
         var email = document.getElementById("email").value;
@@ -66,12 +68,12 @@
         } else {
             document.getElementById("email").setAttribute("style", "border: 1px solid #777777;");
         }
-​
+        
         // if the 3 fields aren't empty enable checkout button
         let fname = checkName(firstName.value);
         let lname = checkName(lastName.value);
         let mail = validateEmail(email);
-​
+        
         if(fname && lname && mail) {
             checkoutBtn.disabled = false;
             document.getElementById('custom-error').style.display = "none";
@@ -79,13 +81,12 @@
             document.getElementById('custom-error').style.display = "block";
         }
     });
-​
+    
     // checkout btn
     checkoutBtn.addEventListener('click', function(){
         document.getElementsByClassName('modal')[0].innerHTML = `<center><h1> Checkout Complete! </h1></center>`;
     });
-​
-​
+    
     // products actions
     let removeBtn = document.getElementsByClassName('remove-item');
     let myFunction = function(event) {
@@ -95,16 +96,16 @@
         target.parentElement.parentElement
         
     };
-​
+    
     for (let i = 0; i < removeBtn.length; i++) {
         removeBtn[i].addEventListener('click', myFunction, false);
         console.log(removeBtn[i]);
     }
-​
+
 })();
 /********************* END MAIN FUNCTION **********************/
-​
-​
+
+
 /********** HELP FUNCTIONS **********/
 function checkName(name) {
     let letters = "^[a-zA-Z]*$";
@@ -114,7 +115,7 @@ function checkName(name) {
         return false;
     }
 }
-​
+
 function validateEmail(email) {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
 }
