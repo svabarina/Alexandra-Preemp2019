d26b4a3d4c7c2e45cda41648d9197a799255f548
Alexandra Barina <alexandra.barina@softvision.ro>
2019-08-01 18:03:22 +0300

    added new functionalities

--- ziuadoi/React/amazing_store/src/Components/MainContent/MainContent.jsx
+++ ziuadoi/React/amazing_store/src/Components/MainContent/MainContent.jsx
@@ -1,37 +1,46 @@
 import React from 'react';
 import './MainContent.css';
 import Sidebar from '../Sidebar/Sidebar';
-
+import Sidead from '../Sidead/Sidead';
+//import Products from '.'
 
 class MainContent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            isToggled: false,
             categories: {
-                FACE:   ['Creams & gels', 'Patches', 'Emulsions', 'Toners', 'Face cleansing', 'Makeup removal', 'Scrubs', 'Peeling', 'Masks'],
-                BODY:   [],
-                FOOT:   [],
-                HANDS:  [],
-                HAIR:   [],
-                MAKEUP: [],
+                Face: {id: 1, listItems:[
+                    "Creams & Gels",
+                    "Patches",
+                    "Emulstions",
+                    "Tonners",
+                    "Face Cleansing",
+                    "Make-up Removal",
+                    "Scrubs",
+                    "Peeling",
+                    "Masks"
+                ]},
+                Body:   {id:2,listItems:["Creams", "Gel Shower"]},
+                Foot:   {id:3, listItems:["Cream"]},
+                Hands:  {id:4, listItems:["Cream","Balsam"]},
+                Hair:   {id:5, listItems:["Shampoo","Balsam","Conditioner"]},
+                Makeup: {id:6, listItems:["Mascara","Eyeliner"]}
             }
         }
     }
 
-    render() {
+    render(){
         return(
-            <>
-                <div>MainContent</div>
-                <Sidebar
-                categories={this.state.categories} 
-                disabled = {false}
-                hidden={false}
-                title={"All categories"}
-                />
-            </>
-
-            )
+            <div className="content">
+            <Sidebar categories={this.state.categories} />
+            <Sidead />
+            {/* <Products /> */}
+            </div>
+        )
     }
+
 }
+
 
 export default MainContent;
---
+++ ziuadoi/React/amazing_store/src/Products/Products.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Products.css';
+
+
+class Products extends React.Component {
+    
+}
+
+
+
+
+
+export default Products;
